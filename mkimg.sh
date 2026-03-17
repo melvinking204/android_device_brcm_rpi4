b13e4a1f0a8c26bde567603e3f9b6a97118d0ec6
Melvin King <mking@starlightunlimited.net>
2026-03-17 12:43:35 +0100

    rpi4: Add hourly timestamp to mkimg script

--- mkimg.sh
+++ mkimg.sh
@@ -26,7 +26,7 @@ for PARTITION in "boot" "system" "vendor"; do
 done
 
 VERSION=RaspberryVanillaAOSP16
-DATE=$(date +%Y%m%d)
+DATE=$(date +%Y%m%d_%H%M)
 TARGET=$(echo ${TARGET_PRODUCT} | sed 's/^aosp_//')
 IMGNAME=${VERSION}-${DATE}-${TARGET}.img
 IMGSIZE=15360000000
